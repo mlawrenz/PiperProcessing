18d955567175d980ec3f4bf861db2a7a1e665c3b
Morgan Lawrenz <mlawrenz@computemaster.localdomain>
2018-07-31 15:26:30 -0700

    add prime input writing for find sasa script

--- find_binding_site_residues_sasa.py
+++ find_binding_site_residues_sasa.py
@@ -11,6 +11,47 @@ from schrodinger.utils import fileutils
 from schrodinger.infra import mm
 from schrodinger.structutils import analyze
 
+def write_prime_inputs(asl, mini=False):
+    if mini==True:
+        ohandle=open('prime_mini_custom.inp', 'w')
+        ohandle.write('''STRUCT_FILE     XXX.mae
+JOB_TYPE        REFINE
+PRIME_TYPE      REAL_MIN
+SELECT  asl = %s
+MINIM_NITER     2
+MINIM_NSTEP     65
+MINIM_RMSG      0.010000
+MINIM_METHOD    au
+USE_CRYSTAL_SYMMETRY    no
+USE_RANDOM_SEED no
+SEED    0
+OPLS_VERSION OPLS3
+EXT_DIEL        80.00
+USE_MEMBRANE    no''' % asl)
+        ohandle.close()
+    else:
+        ohandle=open('prime_hybridmc_custom.inp', 'w')
+        ohandle.write('''STRUCT_FILE     XXX.mae
+JOB_TYPE        REFINE
+PRIME_TYPE      MC
+SELECT  asl = %s
+NSTEPS  25
+PROB_SIDEMC     0.000000
+PROB_RIGIDMC    0.000000
+PROB_HMC        1.000000
+TEMP_HMC        900.000000
+FIND_BEST_STRUCTURE     yes
+NUM_OUTPUT_STRUCT       1
+USE_CRYSTAL_SYMMETRY    no
+USE_RANDOM_SEED no
+SEED    0
+OPLS_VERSION OPLS3
+EXT_DIEL        80.00
+USE_MEMBRANE    no''' % asl)    
+        ohandle.close()
+    return
+
+
 
 
 def filter_residues_by_sasa(struct, st, all_chains_dict, sasa_cutoff, distance, binding_site_residues, writepdb=False):
@@ -131,16 +172,33 @@ all_chains_dict, args.sasa_cutoff, args.distance_cutoff, binding_site_residues)
         ohandle=open('distances_for_restraints.txt', 'w')
         for (x,y) in zip(all_chains_dict[rec_chain], all_chains_dict[lig_chain]):
             ohandle.write('%s-%s\t%s-%s\n' % (rec_chain,x, lig_chain, y))
+        ohandle.close()
+        if args.prime==True:
+            rec_num=[]
+            for (n, x) in enumerate(all_chains_dict[rec_chain]):
+                if x!='NA':
+                    rec_num.append(str(x))
+            lig_num=[]
+            for (n, x) in enumerate(all_chains_dict[lig_chain]):
+                if x!='NA':
+                    lig_num.append(str(x))
+            rec_num=','.join(rec_num)
+            lig_num=','.join(lig_num)
+            asl='((chain %s) AND (res.num %s)) OR ((chain %s) AND (res.num %s))' % (rec_chain, rec_num, lig_chain, lig_num)
+            write_prime_inputs(asl, mini=True)
+            write_prime_inputs(asl, mini=False)
     return
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description='''Find binding site residues accounting for SASA of the atom.''')
     parser.add_argument('-r', dest="rec", help="PDB receptor file for docking, with ligand")
-    parser.add_argument('-l', dest="lig", help="PDB ligand file for docking, with ligand.")
+    parser.add_argument('-l', dest="lig", help="PDB ligand file for docking, with ligand. Can leave this off to just compute SASA list for receptor protein.")
 
     parser.add_argument('-s', dest='sasa_cutoff', type=float, default=50, help="SASA cutoff for binding site residue selection. Default is 50 Angstrom^2")
     parser.add_argument('-d', dest='distance_cutoff', type=float, default=4, help="Receptor-ligand distance cutoff for residue selection. Default is 4.0 Anstrom")
     parser.add_argument('--debug',action='store_true',help="Debug.")
+    parser.add_argument('--prime',action='store_true',help="Write prime outputs with ASL from find SASA")
+
     args = parser.parse_args()
     main(args)
 
